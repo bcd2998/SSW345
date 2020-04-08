212bc70dd0f8ea47590e34145828f1a9f66f756f
Brittany DiFede <bcd2998@gmail.com>
2020-04-08 12:25:58 -0400

    express api

---
+++ express-api/app.js
@@ -0,0 +1,31 @@
+//Require packages and set the port
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3002;
+const app = express();
+const routes = require('./routes/routes');
+
+//Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+// app.get('/', (request, response) => {
+//     response.send({
+//         message: 'Node.js and Express REST API'}
+//     );
+//     //console.log(`URL: ${request.url}`);
+//     //response.send('Hello, Server!');
+// });
+
+//Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
+
+
